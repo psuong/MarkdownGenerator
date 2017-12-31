5f90f6d02e2aaa0cd3732ca99c56ba0472740f88
Porrith Suong <psuong95@gmail.com>
2017-12-30 23:12:13 -0500

    Adds a header.

--- Program.cs
+++ Program.cs
@@ -25,6 +25,7 @@ namespace MarkdownWikiGenerator {
             homeBuilder.Header(1, "References");
             homeBuilder.AppendLine();
 
+
             foreach (var g in types.GroupBy(x => x.Namespace).OrderBy(x => x.Key)) {
                 if (!Directory.Exists(dest)) { Directory.CreateDirectory(dest); }
                 
@@ -36,7 +37,7 @@ namespace MarkdownWikiGenerator {
                 homeBuilder.HeaderWithLink(2, g.Key, g.Key);
                 homeBuilder.AppendLine();
 
-                var sb = new StringBuilder();
+                var index = 1;
                 foreach (var classItem in g.OrderBy(x => x.Name)) {
                     // Make the class a directory
                     var classDirectory = Path.Combine(namespaceDirectory, classItem.BeautifyName.Replace("<", "-").Replace(">", "").Replace(",", "").Replace(" ", "-").ToLower());
@@ -46,14 +47,27 @@ namespace MarkdownWikiGenerator {
                     // Write the contents of the class to the file
                     var file = Path.Combine(classDirectory, "_index.md");
                     Console.WriteLine("Making: " + file);
-                    File.WriteAllText(file, classItem.ToString());
+                    var content = WriteContent(classItem.BeautifyName, index, classItem.ToString());
+                    File.WriteAllText(file, content);
 
+                    index++;
                 }
 
             }
 
             // Gen Home
             File.WriteAllText(Path.Combine(dest, "_index.md"), homeBuilder.ToString());
+        }
+
+        private static string WriteContent(string title, int index, string content) {
+            var sb = new StringBuilder();
+            sb.AppendLine("+++");
+            sb.AppendLine(string.Format("title = \"{0}\"", title));
+            sb.AppendLine(string.Format("weight = {0}", index));
+            sb.AppendLine("+++");
+            sb.AppendLine();
+            sb.Append(content);
+            return sb.ToString();
         }
     }
 }
